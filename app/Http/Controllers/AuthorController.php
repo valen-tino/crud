cf366e714a67ee6463db2b55ba6a513e5d21644c
valentinojehaut <jehaut23@gmail.com>
2022-01-06 13:07:30 +0800

    First Commit
    - Setup the Laravel Project with the name CRUD

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    // Fungsi Ambil Semua Data Artikel Yang Tersimpan
+
+    public function index(){
+        $author=Author::all();
+        $content=['author'=>$author];
+        return $content;
+    }
+
+    // Fungsi Buat Artikel Baru
+
+    public function buatartikelbaru(Request $request){
+        $author=new Author();
+        $author->judulartikel=$request->judulartikel;
+        $author->artikel=$request->artikel;
+        $author->save();
+
+        return " Artikel Berhasil Dibuat ";
+    }
+
+    // Fungsi Update Artikel
+
+    public function updateartikel(Request $request, $id)
+    {
+        $author=Author::find($id);
+        $author->judulartikel=$request->judulartikel;
+        $author->artikel=$request->alamat;
+        $author->save();
+
+        return " Artikel Berhasil Diupdate ";
+    }
+
+    // Fungsi Hapus Artikel
+
+    public function hapusartikel($id)
+    {
+        $author=Author::find($id);
+        $author->delete();
+
+        return " Artikel Berhasil Dihapus ";
+    }
+
+    // Fungsi Baca Artikel
+
+    public function detailartikel($id)
+    {
+        $author=Author::find($id);
+        return $author;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('author', 'AuthorController@index');
+Route::post('author', 'AuthorController@buatartikelbaru');
+Route::get('/author/{id}','AuthorController@detailartikel');
+Route::put('/author/{id}','AuthorController@updateartikel');
+Route::delete('/author/{id}','AuthorController@hapusartikel');
